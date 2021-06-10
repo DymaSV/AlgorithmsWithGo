0c2e854a4f8a71a246d2d8001c0bdb192fdfe482
DymaSv <dymasvyrydonov@gmail.com>
2021-06-10 19:38:38 -0400

    j should go to 0, when it is last value.

--- concatenation/concatenation_test.go
+++ concatenation/concatenation_test.go
@@ -12,7 +12,7 @@ func TestCheckConcatenation(t *testing.T) {
 		str         string
 		expected    []int
 	}{
-		{"123", "111123", []int{3}},
+		{"123", "2a123b321", []int{2}},
 		{"123", "21123a", []int{2}},
 		{"123", "a12321", []int{1}},
 		{"123", "24143424", []int{}},
